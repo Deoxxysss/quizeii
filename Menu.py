8393816051437fd1cd8cd80be94c7b72ade835b4
Abhishek Thakur <abhishek123tcr@gmail.com>
2026-07-19 20:29:24 +0530

    csv file fore upcoming leaderboard

--- Menu.py
+++ Menu.py
@@ -1,21 +1,25 @@
 # Like a main menu of the game
-print('•New Game\n•Settings\n•Leaderboard')
-while True:
-    s = input().lower()
-    if s == "settings":
-        pass
-    elif s == "new game":
-        from basicstructure import play_game
+from basicstructure import play_game
+def Menu():
+    print("=====WELCOME TO QUIZEII=====")
+    print()
+    print()
+    print('•New Game\n•Settings\n•Leaderboard')
+    while True:
+        s = input().lower()
+        if s == "settings":
+            pass
+        elif s == "new game":
 
-        choice = input("Play or Exit? ").lower()
+            choice = input("Play or Exit? ").lower()
 
-        if choice == "play":
-            play_game()
-            
-    elif s == "leaderboard":
-        pass
-    elif s == "break":
-        break
-    else:
-        print("Please Choose from above")
-    
+            if choice == "play":
+                play_game()
+                break
+                
+        elif s == "leaderboard":
+            pass
+        elif s == "break":
+            break
+        else:
+            print("Please Choose from above")

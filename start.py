0b526026d0b44c151874786b8c1ac893202b8bc9
Abhishek Thakur <abhishek123tcr@gmail.com>
2026-07-23 13:05:53 +0530

    Added a start or a login page to be precise

---
+++ start.py
@@ -0,0 +1,10 @@
+from Menu import Menu
+def start():
+    user = input('DO you want to play quizeii:')
+    if user == "yes":
+        print()
+        print()
+        print()
+        Menu()
+    else:
+        return
